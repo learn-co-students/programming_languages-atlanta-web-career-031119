0cbd72056e4909ff6afa94afb1d7571818bcf3fd
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-06 16:05:03 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,25 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {} 
+  languages.each do |language, data|
+    data.each do |attribute, info|
+      
+    if new_hash[attribute] == nil 
+      new_hash[attribute] = info
+      end 
+      new_hash[attribute][:style]=[:oo]
+      if attribute == :clojure || attribute == :erlang || attribute == :scala 
+        new_hash[attribute][:style]=[:functional]
+      elsif attribute == :javascript 
+        new_hash[attribute][:style]=[:oo, :functional]
+      else 
+      end 
+       
+    
+ 
+      
+    end
+  end
+  new_hash
 end
